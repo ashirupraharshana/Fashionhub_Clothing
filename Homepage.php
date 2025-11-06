b5bdf03f83fe8973cc5bc546997ecc10a9a2a956
ashirupraharshana <ashirupraharshana@gmail.com>
2025-11-06 22:26:09 +0530

    Initial commit - FashionHub Clothing website

---
+++ Homepage.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>FashionHub | Home</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<?php include 'Components/navbar.html'; ?>  
+
+<section class="content">
+    <h1>Welcome to FashionHub Clothing Store</h1>
+    <p>Discover our latest fashion trends.</p>
+</section>
+
+</body>
+</html>
